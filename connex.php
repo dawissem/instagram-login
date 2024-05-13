df2513c8fc69436e3bd6b61e0d187b80de165ec3
dawissem <58453871+dawissem@users.noreply.github.com>
2024-05-13 01:20:59 +0100

    Add files via upload

--- connex.php
+++ connex.php
@@ -1,29 +1,29 @@
 <?php
 
-$sername="localhost";
-$username="root";
-$password= "AAA";
-$dbname="ajout";
+// Connect to database
+        $host = "localhost";
+        $userName = "root";
+        $password = "";
+        $dbName = "ajout";
 
-try {
+        $conn = new mysqli($host, $userName, $password, $dbName);
 
-    $conn= new PDO("$mysql:host=$servername,$dbname=$dbname",$username,$password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-   // echo"la connexion a ete bien etablie";
-}
-catch (PDOException $e){
-    echo"la connexion a echoue: " .$e->getMessage();
-}
-if(isset($_POST['Envoyer']))
-{
-    $email = $_POST['email'];
-    $password = $_POST['password'];
+      
+        $email=$_POST['email'];
+        $password=$_POST['password'];
+        $sql = "INSERT INTO users ( email, password) VALUES ( '$email', '$password')";
+        $result = $conn->query($sql);
+        if ($result) {
+            header('Location:Merci.html');
 
-    $sql = ("INSERT INTO `users`(`email`, `password`) VALUES(:email, :password)");
-    $stmt->$conn->prepare($sql);
-    $stmt->bindParam(':email', $email);
-    $stmt->bindParam('', $password);
-    $stmt->execute();
+            $successMsg = "New record created successfully";
+            echo"    $successMsg"  ;   }
+             else {
+            $errorMsg = "Error: ". $sql. "<br>". $conn->error;
+            echo"    $errorMsg"  ;   }
+             
+        
+        $conn->close();
 
-}
+
 ?>
